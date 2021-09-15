c14a4a18f9db8f16ab91fc20293c289164301856
Ptp26 <71855186+Ptp26@users.noreply.github.com>
2021-09-15 17:19:57 -0400

    Grocery App

---
+++ Grocers-MEAN/Frontend/product-grocers/src/app/app.module.ts
@@ -0,0 +1,95 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+// import { ExampleCompComponent } from './example-comp/example-comp.component';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { EmployeeTicketComponent } from './employee-ticket/employee-ticket.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+// import { RaiseTicketComponent } from './raise-ticket/raise-ticket.component';
+// import { EditProjectsComponent } from './edit-projects/edit-projects.component';
+import { ViewRequestsComponent } from './view-requests/view-requests.component';
+// import { EditEmployeesComponent } from './edit-employees/edit-employees.component';
+// import { CartComponent } from './cart/cart.component';
+import { ProfileInfoComponent } from './profile-info/profile-info.component';
+import { SendRequestComponent } from './send-request/send-request.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { UpdateProductComponent } from './product-components/update-product/update-product.component';
+import { DeleteProductComponent } from './product-components/delete-product/delete-product.component';
+import { AddProductComponent } from './product-components/add-product/add-product.component';
+import { RetrieveProductsComponent } from './product-components/retrieve-products/retrieve-products.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavComponent } from './shared/nav/nav.component';
+import { DeleteEmployeeComponent } from './delete-employee/delete-employee.component';
+// import { AddFundsComponent } from './add-funds/add-funds.component';
+import { HomePageComponent } from './components/home-page/home-page.component';
+import { UserComponent } from './components/home-page/user/user.component';
+import { AdminComponent } from './components/home-page/admin/admin.component';
+import { EmployeeComponent } from './components/home-page/employee/employee.component';
+
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginPageComponent,
+
+    ViewRequestsComponent,
+  
+    ProfileInfoComponent,
+    SendRequestComponent,
+    EmployeeTicketComponent,
+    SignUpComponent,
+    UpdateProductComponent,
+    DeleteProductComponent,
+    AddProductComponent,
+    RetrieveProductsComponent,
+    DeleteEmployeeComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    HomePageComponent,
+    UserComponent,
+    AdminComponent,
+    EmployeeComponent,
+
+  ],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      {
+        path: '', pathMatch: 'full', redirectTo: 'login-page'
+      },
+      {path: 'retrieve-products', component: RetrieveProductsComponent},
+      {path: 'add-product', component: AddProductComponent},
+      {path: 'delete-product', component: DeleteProductComponent},
+      {path: 'update-product', component: UpdateProductComponent},
+      {path: 'sign-up', component: SignUpComponent},
+      {path: 'employee-ticket', component: EmployeeTicketComponent},
+      {path: 'view-requests', component: ViewRequestsComponent},
+      {path: 'send-requests', component: SendRequestComponent},
+      {path: 'profile-info', component: ProfileInfoComponent},
+      {path: 'login-page', component: LoginPageComponent},
+ 
+      
+    
+      {path: 'delete-employee', component: DeleteEmployeeComponent},
+      
+      
+      //{path: '/', redirectTo: '/login-page', pathMatch: 'full'}
+      // {path: 'home-page', component: HomePageComponent}
+      
+
+    ]),
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    
+    
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ Grocers-MEAN/Frontend/product-grocers/src/app/login-page/login-page.component.ts
@@ -0,0 +1,79 @@
+import { Component, OnInit } from '@angular/core';
+import { LoginService } from '../services/login.service';
+import { UserService } from '../services/user.service';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { EmployeeService } from '../services/employee.service';
+
+@Component({
+  selector: 'app-login-page',
+  templateUrl: './login-page.component.html',
+  styleUrls: ['./login-page.component.css']
+})
+export class LoginPageComponent implements OnInit {
+  showMe : boolean = true;
+  login = true;
+  attemptsLeft = 3;
+  userID:number;
+  isAdmin:boolean = false;
+  isLoggedIn:boolean = false;
+  constructor(public loginSer:LoginService, public userSer:UserService, public route: Router, public empSer: EmployeeService) { }
+
+  ngOnInit(): void {
+    this.userID = JSON.parse((localStorage.getItem("userId")));
+    if(this.userID.toString() != ""){
+      this.isLoggedIn = true;
+      this.showMe =! this.showMe;
+    }
+    let employee = this.empSer.getEmployee(this.userID).subscribe(employee =>{
+      this.isAdmin = employee[0].isAdmin;
+    });
+
+    //if user -> send to user
+    if(this.isLoggedIn){
+      this.route.navigate(['/home-page']);
+      
+    }
+    // else if(){
+
+    // }
+    // else if(){
+
+    // }
+    // else{
+
+    // }
+
+  }
+
+  checkLogin(loginRef:any){
+    this.loginSer.attemptLogin(loginRef).
+    subscribe(id=>{
+      console.log(id);
+      if(id == ""){
+        console.log("incorrect");
+        this.login = false;
+        let incorrectAttempts = JSON.parse(sessionStorage.getItem("incorrectAttempts"));
+        console.log(incorrectAttempts);
+        if(incorrectAttempts == null){
+          incorrectAttempts = 0;
+        }
+        incorrectAttempts++;
+        this.attemptsLeft --;
+
+        if(incorrectAttempts > 2){
+          this.userSer.lockUser(loginRef);
+        }
+
+        sessionStorage.setItem("incorrectAttempts", JSON.stringify(incorrectAttempts));
+      }
+      else{
+        localStorage.setItem("userId",JSON.stringify(id));
+        sessionStorage.setItem("incorrectAttempts","0");
+        this.route.navigate(['/']);
+      }
+    },error=>console.log(error));
+
+    
+    
+  }
+}
